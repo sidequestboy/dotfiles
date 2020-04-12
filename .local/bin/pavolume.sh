21014554a95c9fb69fed02512c26cd80a0b2184a
Jamie Macdonald <jamie.alban@gmail.com>
2020-04-12 00:19:49 -0700

    Add scripts

--- .local/bin/pavolume.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# requires pulsemixer because pactl and pacmd are limited and difficult.
-# and I don't mind having a good 3rd party CLI installed
-
-pulsemixer --change-volume +10 --max-volume 100
-
