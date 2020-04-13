98a3876f32b99dd39962d1006f3618668383cba4
Jamie Macdonald <jamie.alban@gmail.com>
2020-04-12 19:11:42 -0700

    Rewrite packages script with libinotify
    Use `inotifywait` to subscribe to file changes by pacman and yay.
    Otherwise, update counts every 15 minutes or on USR1 signal.

--- .config/polybar/scripts/updates_arch.sh
+++ .config/polybar/scripts/updates_arch.sh
@@ -1,20 +1,63 @@
 #!/bin/sh
 
-echo "  ?, ?"
+STRING_TEMPLATE=' %s, %s\n'
+TMP_DIR=/tmp/updates_arch
 
-_checkupdates() {
-    echo " $(pacman -Qu | wc -l), $(yay -Qum | wc -l)"
+! [ -d "$TMP_DIR" ] && mkdir "$TMP_DIR"
+echo "?" > "$TMP_DIR/pacman"
+echo "?" > "$TMP_DIR/aur"
+
+_printupdates() {
+    printf "$STRING_TEMPLATE" $(cat "$TMP_DIR/pacman") $(cat "$TMP_DIR/aur")
 }
 
-trap _checkupdates SIGUSR1
-_checkupdates
+_checkpacman() {
+    _pacman_updates=$(pacman -Qu 2> /dev/null | wc -l)
+    [ $? -eq 0 ] && echo "$_pacman_updates" > "$TMP_DIR/pacman"
+}
 
-inotifywait -e close_write,moved_to,create -q -m --exclude '.*\.part' \
-            --format %e /var/lib/pacman/sync |
-while read -r event; do
-    while pgrep pacman 2> /dev/null; do
-        sleep 1
-    done
-    _checkupdates
+_checkaur() {
+    _aur_updates=$(yay -Qum 2> /dev/null | wc -l)
+    [ $? -eq 0 ] && echo "$_aur_updates" > "$TMP_DIR/aur"
+}
+
+_checkupdates() {
+    _checkpacman && _printupdates
+    _checkaur && _printupdates
+}
+
+# watch for pacman file activity
+while inotifywait -e close_write,moved_to,create -q \
+    --exclude '.*\.part' /var/lib/pacman/sync > /dev/null
+do
+    while pgrep pacman 2>&1 > /dev/null; do sleep 1; done
+    _checkpacman && _printupdates
+done &
+pacman_process=$!
+
+# watch for aur file activity
+while inotifywait -e close_write,moved_to,create -q \
+    "$XDG_CACHE_HOME/yay" > /dev/null
+do
+    while pgrep yay 2>&1 > /dev/null; do sleep 1; done
+    _checkaur && _printupdates
+done &
+aur_process=$!
+
+# update on USR1 signal
+trap _checkupdates USR1
+
+# kill inotifywait processes on exit
+# I'm not sure if this is necessary!
+trap "kill $pacman_process; kill $aur_process" EXIT
+
+# initial update
+_printupdates
+_checkupdates
+while :
+do
+    # update every 15min
+    sleep 900 && _checkupdates &
+    wait $!
 done
 
