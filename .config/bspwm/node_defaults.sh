588cdc6f12999b900529b93e5d7120c880849c92
Jamie Macdonald <jamie.alban@gmail.com>
2020-05-05 02:46:11 -0700

    fix typo

--- .config/bspwm/node_defaults.sh
+++ .config/bspwm/node_defaults.sh
@@ -13,8 +13,8 @@ node_class=$(   echo "$xprop_output" | awk -F '"' '{ print $4 }')
 
 rules=$(bspc rule -l)
 
-rules_1=$(echo "$rules" | grep "^$node_class:$instance_name" | awk -F ' => ' '{ print $2 }')
-rules_2=$(echo "$rules" | grep "^\*:$instance_name"          | awk -F ' => ' '{ print $2 }')
+rules_1=$(echo "$rules" | grep "^$node_class:$node_instance" | awk -F ' => ' '{ print $2 }')
+rules_2=$(echo "$rules" | grep "^\*:$node_instance"          | awk -F ' => ' '{ print $2 }')
 rules_3=$(echo "$rules" | grep "^$node_class:\*"             | awk -F ' => ' '{ print $2 }')
 rules_4=$(echo "$rules" | grep "^\*:\*"                      | awk -F ' => ' '{ print $2 }')
 
